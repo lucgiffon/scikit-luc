37bd0d46342e9a7ce2ecc5418c4fab3460cbc999
Luc Giffon <luc.giffon@ens-lyon.fr>
2021-06-09 11:06:51 +0200

    add count option for csvgatherer

--- skluc/tools/csvgatherer.py
+++ skluc/tools/csvgatherer.py
@@ -6,7 +6,8 @@ import click_pathlib
 @click.command()
 @click.argument("results_dir", type=click_pathlib.Path(exists=True, dir_okay=True, resolve_path=True))
 @click.argument("output_dir", type=click_pathlib.Path(exists=False, dir_okay=True, resolve_path=True))
-def main(results_dir, output_dir):
+@click.option("-c", "--count", is_flag=True, help="Print the number of lines in the resulting csv.")
+def main(results_dir, output_dir, count):
 
     files = [x for x in results_dir.glob('**/*') if x.is_file()]
     lst_df = []
@@ -19,8 +20,10 @@ def main(results_dir, output_dir):
             lst_other_files.append(csv_filename)
 
     total_df = pd.concat(lst_df)
+    if count:
+        print(len(total_df))
     output_dir.mkdir(parents=True, exist_ok=True)
-    total_df.to_csv(output_dir / "raw_results.csv", index=False)
+    total_df.to_csv(output_dir / "gathered_results.csv", index=False)
 
 
 if __name__ == "__main__":

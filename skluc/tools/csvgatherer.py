0d86cfd11e8d572bdd19fe68a7f24c3ed44698fd
Luc Giffon <luc.giffon@ens-lyon.fr>
2021-06-14 18:07:02 +0200

    add delete option with safety guards

--- skluc/tools/csvgatherer.py
+++ skluc/tools/csvgatherer.py
@@ -7,25 +7,44 @@ import click_pathlib
 @click.argument("results_dir", type=click_pathlib.Path(exists=True, dir_okay=True, resolve_path=True))
 @click.argument("output_dir", required=False, default=None, type=click_pathlib.Path(exists=False, dir_okay=True, resolve_path=True))
 @click.option("-c", "--count", is_flag=True, help="Print the number of lines in the resulting csv.")
-def main(results_dir, output_dir, count):
+@click.option("-d", "--delete", is_flag=True, help="Delete files after gathering.")
+def main(results_dir, output_dir, count, delete):
     if output_dir is None:
         output_dir = results_dir
+    name_gathered_file = "gathered_results.csv"
 
     files = [x for x in results_dir.glob('**/*') if x.is_file()]
     lst_df = []
     lst_other_files = []
+    real_count = 0
     for csv_filename in files:
+        if csv_filename.name == name_gathered_file:
+            continue
+
         if csv_filename.suffix == ".csv":
             df = pd.read_csv(csv_filename)
             lst_df.append(df)
+            real_count += 1
         else:
             lst_other_files.append(csv_filename)
+
+    if real_count < 2:
+        raise ValueError(f"{real_count} csv file found. Should be at least 2. Nothing done.")
 
     total_df = pd.concat(lst_df)
     if count:
         print(len(total_df))
     output_dir.mkdir(parents=True, exist_ok=True)
-    total_df.to_csv(output_dir / "gathered_results.csv", index=False)
+    if len(total_df) > 0:
+        total_df.to_csv(output_dir / name_gathered_file, index=False)
+    else:
+        raise ValueError("No csv file found in the directory or they were empty")
+
+    if delete and len(total_df) > 0:
+        for csv_filename in files:
+            if csv_filename.suffix == ".csv" and csv_filename.name != name_gathered_file:
+                csv_filename.unlink()
+
 
 
 if __name__ == "__main__":
